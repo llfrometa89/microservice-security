307ff922756048cea7522db6d6765422bd2d00ca
Scala Steward <llfrometa@gmail.com>
2020-02-13 22:54:16 +0100

    Update cats-effect to 2.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
 
     val catsVersion       = "2.0.0"
-    val catsEffectVersion = "2.0.0"
+    val catsEffectVersion = "2.1.1"
     val scalaTestVersion  = "3.2.0-M1"
     val monocleVersion    = "2.0.0"
     val scalaCheckVersion = "1.14.2"

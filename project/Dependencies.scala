de7ee310fbabffd30cb5207170c932e4417d7f28
Scala Steward <llfrometa@gmail.com>
2020-02-13 22:51:45 +0100

    Update better-monadic-for to 0.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     val awsSdkVersion     = "1.11.656"
     val pureConfigVersion = "0.12.1"
     val kindProjector     = "0.9.8"
-    val betterMonadicFor  = "0.3.0-M4"
+    val betterMonadicFor  = "0.3.1"
     val catsMeowMtl       = "0.2.0"
   }
 
